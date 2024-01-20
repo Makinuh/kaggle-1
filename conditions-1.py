5fb9c861c210516b7fd9038dcecf54ad98f2049b
Harv <brbdicals@users.noreply.github.com>
2024-01-20 02:29:25 -0600

    vim update filename using 'mv'

--- conditions-1.py
+++
@@ -1,8 +0,0 @@
-def evaluate_temp(temp):
-    # Set an initial message
-    message = "Normal temperature."
-    # Update value of message only if temperature greater than 38
-    if temp > 38:
-        message = "Fever!"
-    return message
-
