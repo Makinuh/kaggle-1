656abd913c9e77cb3f4c98d544d3edd34ace717a
Makinuh <5678571+Makinuh@users.noreply.github.com>
2024-01-20 02:15:30 -0600

    Create conditions-1.py

---
+++ conditions-1.py
@@ -0,0 +1,8 @@
+def evaluate_temp(temp):
+    # Set an initial message
+    message = "Normal temperature."
+    # Update value of message only if temperature greater than 38
+    if temp > 38:
+        message = "Fever!"
+    return message
+
